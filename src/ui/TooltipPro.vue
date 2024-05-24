469765597e8752f1328b31ae796e6faca752959f
Ted not teddy <tedxweb@outlook.com>
2024-05-24 14:31:04 +0800

    feat: add Tooltip and TooltipPro component

---
+++ src/ui/TooltipPro.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from 'oooo-components/ui/tooltip'
+
+defineProps<{
+  message: string
+}>()
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{{ message }}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
