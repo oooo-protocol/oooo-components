e4aa7cc15fc75963f12fc712401cf2e6ce1631b2
Ted not teddy <tedxweb@outlook.com>
2024-06-04 15:00:57 +0800

    feat: add chain config constant

---
+++ src/lib/chain-config.ts
@@ -0,0 +1,223 @@
+export const BEVM_TESTNET = {
+  chainId: '0x2cef',
+  chainName: 'BEVM Testnet',
+  rpcUrls: ['https://testnet.bevm.io'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://scan-testnet.bevm.io']
+}
+
+export const BEVM_LIVENET = {
+  chainId: '0x2ced',
+  chainName: 'BEVM',
+  rpcUrls: ['https://rpc-mainnet-1.bevm.io/'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://scan-mainnet.bevm.io/']
+}
+
+export const BEVM_CANARY_TESTNET = {
+  chainId: '0x5de',
+  chainName: 'BEVM Canary Testnet',
+  rpcUrls: ['https://canary-testnet.bevm.io'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://scan-canary-testnet.bevm.io']
+}
+
+export const BEVM_CANARY_LIVENET = {
+  chainId: '0x5dd',
+  chainName: 'BEVM Canary',
+  rpcUrls: ['https://rpc-canary-1.bevm.io/'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://scan-canary.bevm.io/']
+}
+
+export const B2_TESTNET = {
+  chainId: '0x463',
+  chainName: 'B2 Testnet',
+  rpcUrls: ['https://b2-testnet.alt.technology'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://testnet-explorer.bsquared.network']
+}
+
+export const B2_LIVENET = {
+  chainId: '0xdf',
+  chainName: 'B2 Mainnet',
+  rpcUrls: ['https://rpc.bsquared.network', 'https://b2-mainnet.alt.technology'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://explorer.bsquared.network/']
+}
+
+export const BTC_TESTNET = {
+  blockExplorerUrls: ['https://mempool.space/testnet']
+}
+
+export const BTC_LIVENET = {
+  blockExplorerUrls: ['https://mempool.space']
+}
+
+export const MERLIN_TESTNET = {
+  chainId: '0xa7b14',
+  chainName: 'Merlin Testnet',
+  rpcUrls: ['https://testnet-rpc.merlinchain.io'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://testnet-scan.merlinchain.io']
+}
+
+export const MERLIN_LIVENET = {
+  chainId: '0x1068',
+  chainName: 'Merlin Mainnet',
+  rpcUrls: ['https://rpc.merlinchain.io'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://scan.merlinchain.io']
+}
+
+export const ROOTSTOCK_TESTNET = {
+  chainId: '0x1f',
+  chainName: 'RSK Testnet',
+  rpcUrls: ['https://public-node.testnet.rsk.co'],
+  nativeCurrency: {
+    name: 'tRBTC',
+    symbol: 'tRBTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://explorer.testnet.rsk.co']
+}
+
+export const ROOTSTOCK_LIVENET = {
+  chainId: '0x1e',
+  chainName: 'RSK Mainnet',
+  rpcUrls: ['https://public-node.rsk.co'],
+  nativeCurrency: {
+    name: 'RBTC',
+    symbol: 'RBTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://explorer.rsk.co']
+}
+
+export const BITLAYER_TESTNET = {
+  chainId: '0x3106a',
+  chainName: 'Bitlayer Testnet',
+  rpcUrls: ['https://testnet-rpc.bitlayer.org'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://testnet-scan.bitlayer.org']
+}
+
+export const BITLAYER_LIVENET = {
+  chainId: '0x310c5',
+  chainName: 'Bitlayer Mainnet',
+  rpcUrls: ['https://rpc.bitlayer.org', 'https://rpc.bitlayer-rpc.com', 'https://rpc.ankr.com/bitlayer'],
+  nativeCurrency: {
+    name: 'BTC',
+    symbol: 'BTC',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://www.btrscan.com']
+}
+
+export const BOB_TESTNET = {
+  chainId: '0x6f',
+  chainName: 'BOB Testnet',
+  rpcUrls: ['https://testnet.rpc.gobob.xyz/'],
+  nativeCurrency: {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://testnet-explorer.gobob.xyz/']
+}
+
+export const BOB_LIVENET = {
+  chainId: '0xed88',
+  chainName: 'BOB Mainnet',
+  rpcUrls: ['https://rpc.gobob.xyz/'],
+  nativeCurrency: {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://explorer.gobob.xyz/']
+}
+
+export const BSC_TESTNET = {
+  chainId: '0x61',
+  chainName: 'BSC Testnet',
+  rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/', 'http://data-seed-prebsc-1-s2.binance.org:8545/'],
+  nativeCurrency: {
+    name: 'tBNB',
+    symbol: 'tBNB',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://testnet.bscscan.com']
+}
+
+export const BSC_LIVENET = {
+  chainId: '0x38',
+  chainName: 'BSC Mainnet',
+  rpcUrls: ['https://bsc-dataseed1.binance.org/', 'https://bsc-dataseed1.defibit.io/'],
+  nativeCurrency: {
+    name: 'BNB',
+    symbol: 'BNB',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://bscscan.com/']
+}
+
+export const ARBITRUM_TESTNET = {
+  chainId: '0x66eee',
+  chainName: 'Arbitrum Sepolia',
+  rpcUrls: ['https://sepolia-rollup.arbitrum.io/rpc'],
+  nativeCurrency: {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://sepolia.arbiscan.io']
+}
+
+export const ARBITRUM_LIVENET = {
+  chainId: '0xa4b1',
+  chainName: 'Arbitrum One',
+  rpcUrls: ['https://arb1.arbitrum.io/rpc'],
+  nativeCurrency: {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  blockExplorerUrls: ['https://arbiscan.io/']
+}
