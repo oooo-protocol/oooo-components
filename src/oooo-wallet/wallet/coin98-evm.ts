29f76f4a1886ff4fb66e751b8dcee8c7afce3465
Ted not teddy <tedxweb@outlook.com>
2024-08-09 19:06:48 +0800

    fix: Coin98 wallet uninstall prompt error

--- src/oooo-wallet/wallet/coin98-evm.ts
+++ src/oooo-wallet/wallet/coin98-evm.ts
@@ -3,7 +3,7 @@ import { EthereumWallet } from './ethereum'
 
 export class Coin98EthereumWallet extends EthereumWallet {
   get provider () {
-    if (window.coin98 == null) throw new NoAlarmException('Please install OKX Wallet')
+    if (window.coin98 == null) throw new NoAlarmException('Please install Coin98 Wallet')
     return window.coin98.provider
   }
 }
